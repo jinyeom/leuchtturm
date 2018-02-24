9db831d66df68cb3526e22ae25a4ef98122dacdf
jinyeom <jinyeom95@gmail.com>
2018-02-24 01:19:06 -0600

    progress

--- scripts/gridworld.py
+++ scripts/gridworld.py
@@ -23,10 +23,10 @@ class GridWorld(object):
     def __init__(self, m):
         if not isinstance(m, np.ndarray) or len(m.shape) != 2:
             raise TypeError("GridWorld map must be a 2D numpy.ndarray")
-        r, c = m.shape
         self._m = m
-        self._V = np.zeros((r, c)) # Values for each state
-        self._Q = np.zeros((r, c, 4)) # Q values for 4 directions in each state
+        self._r, self._c = m.shape
+        self._V = np.zeros((self._r, self._c))
+        self._Q = np.zeros((self._r, self._c, 4))
        
     def __getitem__(self, i, j):
         return self._m[i][j]
@@ -43,30 +43,32 @@ class GridWorld(object):
     def Q(self):
         return deepcopy(self._Q)
     
-    def get_V(i, j):
-        """ Return the value of state (i, j) """
-        return self._V[i][j]
-
-    def get_Q(i, j, a):
-        """ Return the Q-value of state (i, j) for action a """
-        return self._Q[i][j][a]
+    @property
+    def S(self):
+        """ Return a list of all possible states. """
+        r, c = self._m.shape
+        return [(i, j) for i in range(r) for j in range(c)]
     
-    def set_V(i, j, v):
-        self._V[i][j] = v
+    @property
+    def shape(self):
+        return self._r, self._c
     
-    def set_Q(i, j, a, q):
-        self._Q[i][j][a] = q
-        self._V[i][j] = np.max(self._Q[i][j])
+    def set_V(self, s, v):
+        self._V[s] = v
+    
+    def set_Q(self, s, a, q):
+        self._Q[s][a] = q
+        self._V[s] = np.max(self._Q[s])
           
-    def R(self, i, j):
+    def R(self, s):
         """ Reward function """
-        return [-1, -1, 500, -500][self._m[i][j]]
+        return [-1, -1, 50, -50][self._m[s]]
     
-    def T(self, i, j, a):
+    def T(self, s, a):
         """ Transition function """
         if a not in [UP, RIGHT, DOWN, LEFT]:
             raise ValueError("invalid direction")
-        next_i, next_j = i, j
+        next_i, next_j = s
         if a == UP:
             next_i -= 1
         elif a == RIGHT:
@@ -76,17 +78,22 @@ class GridWorld(object):
         elif a == LEFT:
             next_j -= 1
         # if the next position is invalid, revert back.
-        if next_i >= r or next_j >= c or self._m[next_i][next_j] == WALL:
-            next_i, next_j = i, j
+        if next_i >= self._r or next_j >= self._c or self._m[next_i][next_j] == WALL:
+            next_i, next_j = s
         return next_i, next_j
+    
+    def done(self, s):
+        """ Check if the argument state is a terminal state. """
+        if self._m[s] == GOAL or self._m[s] == FIRE:
+            return True
+        return False
         
     def show(self):
         """ Print the map """
         def sym(i, j):
             return [" ", "#", "o", "x"][self._m[i][j]]
-        r, c = self._m.shape
-        print("-".join(["+" for j in range(c + 1)]))
-        for i in range(r):
-            print("|%s|" % "|".join(["%s" % sym(i, j) for j in range(c)]))
-            print("-".join(["+" for j in range(c + 1)]))
+        print("-".join(["+" for j in range(self._c + 1)]))
+        for i in range(self._r):
+            print("|%s|" % "|".join(["%s" % sym(i, j) for j in range(self._c)]))
+            print("-".join(["+" for j in range(self._c + 1)]))
                 
